78ca41b90cd3c25f38938a58f92a988eb83f11ac
kyungtaeChoi <kmam147@gmail.com>
2025-05-02 01:25:33 +0900

    chore: 초기 Backend (스프링부트) 추가

---
+++ Backend/src/main/java/com/cqrsstudy/cqrsdohi/CqrsdohiApplication.java
@@ -0,0 +1,13 @@
+package com.cqrsstudy.cqrsdohi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CqrsdohiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CqrsdohiApplication.class, args);
+	}
+
+}
---
+++ Backend/src/test/java/com/cqrsstudy/cqrsdohi/CqrsdohiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cqrsstudy.cqrsdohi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CqrsdohiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
